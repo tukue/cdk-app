a10a1c8f035171942dad6f5c3f0fdbeeb398d937
Tukue Gebregergis <tukue.geb@gmail.com>
2024-04-30 17:28:23 +0200

    updates for serverless app

--- lib/cdk-app-stack.ts
+++ lib/cdk-app-stack.ts
@@ -1,13 +1,42 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
+import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway'
 
-export class CdkAppStack extends cdk.Stack {
+export class CdkAppStack extends Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //define dynamodb table
+const dynamodb_table = new dynamodb.Table(this, "Table", {
+  partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+  removalPolicy: RemovalPolicy.DESTROY
+  }
+  ) 
+  
+  //define lambda function and regeference function file
+  const lambda_backend = new NodejsFunction(this, "function", {
+    runtime: lambda.Runtime.NODEJS_16_X, 
+  tracing: lambda.Tracing.ACTIVE,
+  environment: {
+    DYNAMODB: dynamodb_table.tableName
+  },
+})
 
+dynamodb_table.grantReadData(lambda_backend.role!)
+
+const api = new apigateway.RestApi(this, "RestAPI", {
+  deployOptions: {
+    dataTraceEnabled: true,
+    tracingEnabled: true
+  },
+})
+
+const endpoint = api.root.addResource("scan")
+const endpointMethod=endpoint.addMethod("GET",new apigateway.LambdaIntegration(lambda_backend))
     // example resource
     // const queue = new sqs.Queue(this, 'CdkAppQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
